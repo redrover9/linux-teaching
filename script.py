0fe2436a057ed66aa6582a2d6a9a6e267626cbd1
Grace Thompson <grace.thompson97@gmail.com>
2021-02-14 13:21:39 -0800

    set pings to 3

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 from subprocess import *
-server1_status = check_output(["ping", "server1.example.com"])
-server2_status = check_output(["ping", "server2.example.com"])
-server3_status = check_output(["ping", "server3.example.com"])
-wkstn1_status = check_output(["ping", "wkstn1.example.com"])
+server1_status = check_output(["ping", "-c", "3 ", "server1.example.com"])
+server2_status = check_output(["ping", "-c", "3 ", "server2.example.com"])
+server3_status = check_output(["ping", "-c", "3 ", "server3.example.com"])
+wkstn1_status = check_output(["ping", "-c", "3 ", "wkstn1.example.com"])
