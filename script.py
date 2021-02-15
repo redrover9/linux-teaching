ad23ec8bb2c8e6dccb49230f868b1d17d18fa7a0
Grace Thompson <grace.thompson97@gmail.com>
2021-02-14 16:32:59 -0800

    ready for wednesday!

--- script.py
+++ script.py
@@ -1,17 +1,17 @@
 from subprocess import *
 try: 
-	server1_status = check_output(["ping", "-c", "3 ", "server1.example.com"])
-except subprocess.CalledProcessError as server1_error:
+	server1_status = check_output(["ping", "-c", "3", "server1.example.com"])
+except CalledProcessError as server1_error:
 	print(server1_error)
 try: 
-	server2_status = check_output(["ping", "-c", "3 ", "server2.example.com"])
-except subprocess.CalledProcessError as server2_error:
+	server2_status = check_output(["ping", "-c", "3", "server2.example.com"])
+except CalledProcessError as server2_error:
 	print(server2_error)
 try: 
-	server3_status = check_output(["ping", "-c", "3 ", "server3.example.com"])
-except subprocess.CalledProcessError as server3_error:
+	server3_status = check_output(["ping", "-c", "3", "server3.example.com"])
+except CalledProcessError as server3_error:
 	print(server3_error)
 try: 
-	wkstn1_status = check_output(["ping", "-c", "3 ", "wkstn1.example.com"])
-except subprocess.CalledProcessError as wkstn1_error:
+	wkstn1_status = check_output(["ping", "-c", "3", "wkstn1.example.com"])
+except CalledProcessError as wkstn1_error:
 	print(wkstn1_error)
